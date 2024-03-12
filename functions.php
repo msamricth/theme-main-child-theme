9d815136ed3d22610d322e90053366ac1afb2888
msamricth <emm.talarico@gmail.com>
2024-03-12 14:15:47 -0400

    Depersonalizing The child theme - didn't get a chance to make a git update before making alot of changes

--- functions.php
+++ functions.php
@@ -6,12 +6,3 @@
 
  $theme_enqueue = __DIR__ . '/inc/enqueue.php';
  if ( is_readable( $theme_enqueue ) ) {	require_once $theme_enqueue;}
- 
- 
- 
- $theme_forms = __DIR__ . '/inc/forms.php';
- if ( is_readable( $theme_forms ) ) {	require_once $theme_forms;}
- 
- 
- 
-  
